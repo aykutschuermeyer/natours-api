7d14ed10c6b0e895ef116682289c687676dc290d
Aykut Schürmeyer <aykutschuermeyer@gmail.com>
2020-10-07 14:23:22 +0200

    different errors for prod and dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,23 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 500;
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    error: err
+  });
+};
+
+const sendErrorProd = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message
   });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 500;
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
