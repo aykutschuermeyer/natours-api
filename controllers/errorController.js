707114a936530a7f9dab184eb5417312d17ffd92
Aykut Schürmeyer <aykutschuermeyer@gmail.com>
2020-10-09 18:21:45 +0200

    error handling finished

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,22 @@
+const AppError = require('../utils/appError');
+
+const handleCastError = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFields = err => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Field ${value} already exists`;
+  return new AppError(message, 400);
+};
+
+const handleValidationError = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data: ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     error: err
@@ -26,6 +45,18 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') {
+      error = handleCastError(error);
+    }
+    if (error.code === 11000) {
+      error = handleDuplicateFields(error);
+    }
+    if (error.name === 'ValidationError') {
+      error = handleValidationError(error);
+    }
+
+    sendErrorProd(error, res);
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('Shutting down gracefully...');
+  process.exit();
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -18,6 +24,14 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Shutting down gracefully...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
